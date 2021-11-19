3adf70b53ff889a3861ddd7074ec7c388b9430c8
Basil El-Hindi <basilelhindi@gmail.com>
2021-11-19 17:46:27 -0600

    Added level controller to restart and quit level, and added first person mouse movements to look around

---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelController : MonoBehaviour
+{
+    // On start, lock and hide mouse cursor
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Backspace))
+        {
+            RestartGame();
+        }
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            QuitLevel();
+        }
+    }
+
+    void RestartGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    void QuitLevel()
+    {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    // Inspector fields
+    [Header("Mechanics")]
+    [SerializeField] float _mouseSens = 200.0f;
+
+    [Header("Components")]
+    [SerializeField] Transform _playerTransform;
+
+    // Class variables
+    float _xRotation = 0.0f;
+
+    void Update()
+    {
+        // Capture mouse input on x and y axes
+        float mouseX = Input.GetAxis("Mouse X") * _mouseSens * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * _mouseSens * Time.deltaTime;
+
+        // Rotate the player in the y direction clamped between +/- z
+        _xRotation -= mouseY;
+        _xRotation = Mathf.Clamp(_xRotation, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(_xRotation, 0f, 0f);
+
+        // Rotate the player in the x direction
+        _playerTransform.Rotate(Vector3.up * mouseX);
+    }
+}
