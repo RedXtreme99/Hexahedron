df431b27d031383c6c0d61a6d39f4a40e0e4dec6
Basil El-Hindi <basilelhindi@gmail.com>
2021-12-01 23:49:09 -0600

    Created mote object and script, imported probuilder, created materials, created player inventory

---
+++ Assets/Scripts/Mote.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mote : MonoBehaviour
+{
+    // Components
+    [SerializeField] Material _lightMaterial;
+    [SerializeField] Material _darkMaterial;
+
+    // Class Variables
+    MeshRenderer _renderer;
+    bool _dark = false;
+
+    void Awake()
+    {
+        _renderer = GetComponent<MeshRenderer>();
+        SetDark(true);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        PlayerInventory inventory = other.GetComponent<PlayerInventory>();
+        if(inventory != null)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void SetDark(bool dark)
+    {
+        _dark = dark;
+        if(dark)
+        {
+            _renderer.material = _darkMaterial;
+        }
+        else
+        {
+            _renderer.material = _lightMaterial;
+        }
+    }
+}
